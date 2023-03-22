224d69cb428047796045090a44601ba7d0f169fe
Nicolas Poch <nicopoch25@gmail.com>
2023-03-22 20:58:40 -0300

    Ej de minut. actualizado

--- Guia3Ej_extra1/src/guia3ej_extra1/Guia3Ej_extra1.java
+++ Guia3Ej_extra1/src/guia3ej_extra1/Guia3Ej_extra1.java
@@ -1,9 +1,9 @@
 /*
  Dado un tiempo en minutos, calcular su equivalente en días y horas. Por ejemplo, si el usuario ingresa 1600 minutos,
 el sistema debe calcular su equivalente: 1 día, 2 horas. */
+
 package guia3ej_extra1;
 
-import static java.lang.Math.abs;
 import java.util.Scanner;
 
 public class Guia3Ej_extra1 {
@@ -14,10 +14,10 @@ public class Guia3Ej_extra1 {
          double minutos, horas, dias;
          System.out.println("Ingrese la cantidad de minutos ");
          minutos = leer.nextDouble();
-        horas = minutos/60;
-        dias= horas/24;
-        minutos = 60*(horas-Math.abs(horas)); 
-         System.out.println("Dias : " + dias + " horas : " + horas%24 + " minutos: " + minutos); 
+        horas = (minutos-(minutos%60))/60;
+        dias= (horas-(horas%24))/24;
+        minutos = minutos%60; 
+         System.out.println("Dias : " + dias + " horas : " + (horas-(24*dias)) + " minutos: " + minutos); 
      }
                  
     }
